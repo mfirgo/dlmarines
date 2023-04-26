899cd300d69885796ef2f00c1afe5ee8def56825
Martyna Firgolska <mafirgolska@gmail.com>
2023-04-26 22:00:35 +0200

    Add docstrings for data processing pipeline

--- src/dlmarines/pipelines/data_processing/nodes.py
+++ src/dlmarines/pipelines/data_processing/nodes.py
@@ -6,12 +6,29 @@ from zipfile import ZipFile
 
 
 def unzip_dataset():
+    """Unzips the dataset.
+
+    Note that for this function to work correctly the sea animals dataset 
+    should be already downloaded into `data/01_raw/sea-animals-image-dataste.zip` 
+    either manually or via running data_downloading pipeline.
+
+    Returns:
+        string: 'unzipped'
+    """
     with ZipFile('data/01_raw/sea-animals-image-dataste.zip', 'r') as f:
         f.extractall('data/01_raw')
-    
+    # TODO: return path to unzipped dataset
     return 'unzipped'
 
 def load_dataset(unzipped):
+    """Loades dataset as ImageDataSet.
+
+    Args:
+        unzipped (any): indicates that the previous node has been run
+
+    Returns:
+        PartitionedDataSet: dataset loaded from "data/01_raw/"
+    """
     dataset = PartitionedDataSet(
        path="data/01_raw/",
        dataset="pillow.ImageDataSet",
@@ -20,6 +37,14 @@ def load_dataset(unzipped):
     return dataset
 
 def preprocess_dataset(dataset):
+    """Preprocesses dataset. Each image in dataset is resized and transformed to tensor.
+
+    Args:
+        dataset (PartitionedDataSet): Loaded image dataset
+
+    Returns:
+        PartitionedDataSet: transformed dataset
+    """
     transform = torchvision.transforms.Compose([
         # TODO: add size to params
         torchvision.transforms.Resize((128, 128)),
